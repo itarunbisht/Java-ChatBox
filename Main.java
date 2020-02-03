f99d0e8804512848c5dd050103ed67ee0c98c92f
itarunbisht <44253228+itarunbisht@users.noreply.github.com>
2020-02-03 20:28:10 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+package guichat;
+
+public class Main 
+{
+    public static void main(String [] args)
+    {
+        MainFrame mainFrame = new MainFrame(); //create the MainFrame object
+        Thread thread  = new Thread(mainFrame); //run in a new thread
+        thread.start(); //start the thread
+    }
+}
